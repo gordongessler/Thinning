28b1c4d8383c09239330bff6ce9ebe9feac137ff
Maciej Sobczyński <gryfon1997@gmail.com>
2019-01-17 13:50:48 +0100

    Some more setup + added some utility functions

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,52 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
 public class Main {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws java.io.IOException {
+        File f = new File("input.png");
+        BufferedImage in = ImageIO.read(f);
+        kmm(threshold(in, 1));
     }
 
     //TODO: KMM
     //TODO: K3M
 
+    private static void kmm(BufferedImage in){
+
+    }
+
+    // Thresholding
+    private static BufferedImage threshold(BufferedImage in, int threshold) {
+        BufferedImage out = new BufferedImage(in.getWidth(), in.getHeight(), in.getType());
+        in = grayscale(in);
+        for (int i = 0; i < in.getHeight(); i++) {
+            for (int j = 0; j < in.getWidth(); j++) {
+                int r = getR(in.getRGB(j, i));
+                if (r >= threshold) {
+                    r = 255;
+                } else {
+                    r = 0;
+                }
+                out.setRGB(j, i, toRGB(r, r, r));
+            }
+        }
+        return out;
+    }
+
+
+    // Grayscale
+    private static BufferedImage grayscale(BufferedImage in) {
+        BufferedImage out = new BufferedImage(in.getWidth(), in.getHeight(), in.getType());
+        for (int i = 0; i < in.getHeight(); i++) {
+            for (int j = 0; j < in.getWidth(); j++) {
+                int avg = (getR(in.getRGB(j, i)) + getB(in.getRGB(j, i)) + getG(in.getRGB(j, i))) / 3;
+                out.setRGB(j, i, toRGB(avg, avg, avg));
+            }
+        }
+        return out;
+    }
 
     // Methods provided in the intro document
     private static int getR(int in) {
