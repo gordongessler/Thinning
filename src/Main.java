1b9167a1e73bab2041b7374fe6a95da47fca7332
Maciej Sobczyński <gryfon1997@gmail.com>
2019-01-17 22:11:04 +0100

    Sixth step

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import com.sun.deploy.util.ArrayUtil;
+import sun.plugin.javascript.navig.Array;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -18,7 +22,7 @@ public class Main {
     //TODO: KMM
     //TODO: K3M
 
-    private static void kmm(BufferedImage in){
+    private static void kmm(BufferedImage in) throws IOException {
 
         //Represent the output image as a 2D array
         int width = in.getWidth(), height=in.getHeight();
@@ -102,6 +106,64 @@ public class Main {
         System.out.println("State of the mask after the fifth step");
         printMask(mask);
 
+        //Step 6
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (mask[j][i]==2){
+                    int weight=pixelWeight(addMaskZeroPadding(mask),i+1,j+1);
+                  //  System.out.println(weight);
+                    if(deletionArray.contains(weight)){ mask[j][i]=0;}else{mask[j][i]=1;}
+                }
+            }
+        }
+
+
+
+        ColorModel cm = in.getColorModel();
+        boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
+        WritableRaster raster = in.copyData(null);
+        BufferedImage out = new BufferedImage(cm, raster, isAlphaPremultiplied, null);
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if(mask[j][i]>0) out.setRGB(i,j,toRGB(0,0,0));
+            }
+        }
+        ImageIO.write(out, "png", new File("out.png"));
+
+    }
+
+    private static final ArrayList<Integer> deletionArray = new ArrayList(Arrays.asList(3, 5, 7, 12, 13, 14,
+            15, 20, 21, 22, 23, 28, 29, 30, 31, 48, 52, 53, 54, 55, 56, 60, 61, 62, 63, 65, 67, 69, 71, 77, 79, 80,
+            81, 83, 84, 85, 86, 87, 88, 89, 91, 92, 93, 94, 95, 97, 99, 101, 103, 109, 111, 112,
+            113, 115, 116, 117, 118, 119, 120, 121, 123, 124, 125, 126, 127, 131, 133, 135, 141,
+            143, 149, 151, 157, 159, 181, 183, 189, 191, 192, 193, 195, 197, 199, 205, 207,
+            208, 209, 211, 212, 213, 214, 215, 216, 217, 219, 220, 221, 222, 223, 224, 225, 227,
+            229, 231, 237, 239, 240, 241, 243, 244, 245, 246, 247, 248, 249, 251, 252, 253, 254, 255));
+
+    //Calculate pixel weight
+    private static int pixelWeight(int[][] mask, int x, int y){
+        int[] neighbours = new int[8];
+        int[] valArray ={128,1,2,4,8,16,32,64};
+        int weight=0;
+
+        neighbours[0]=mask[y-1][x-1];
+        neighbours[1]=mask[y-1][x];
+        neighbours[2]=mask[y-1][x+1];
+        neighbours[3]=mask[y][x+1];
+        neighbours[4]=mask[y+1][x+1];
+        neighbours[5]=mask[y+1][x];
+        neighbours[6]=mask[y+1][x-1];
+        neighbours[7]=mask[y][x-1];
+
+
+            for(int i=0;i<8;i++){
+                if(neighbours[i]!=0)weight+=valArray[i];
+            }
+
+        if(weight>0){
+           // System.out.println("DEBUG: " + Arrays.toString(neighbours) + " |  "+Arrays.toString(valArray));
+        }
+        return weight;
     }
 
     //Count sticking neighbours that are not 0
